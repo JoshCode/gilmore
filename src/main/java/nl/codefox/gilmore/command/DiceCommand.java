f24b945c076fcac1bbf2d5c1f5e56f90a169ea5d
Tijmen van der Kemp <tvdkemp@gmail.com>
2016-06-01 11:34:11 +0200

    cleaned up the code
    exploding die now explodes on all crits, not only on max value

--- src/main/java/nl/codefox/gilmore/command/DiceCommand.java
+++ src/main/java/nl/codefox/gilmore/command/DiceCommand.java
@@ -8,7 +8,14 @@ public class DiceCommand extends GilmoreCommand
 
     public DiceCommand() 
     {
-        super("Rolls dice based on input", "Usage: ![roll|dice] [value]", 2, null, "!roll", "!dice");
+        super("Rolls dice based on input", "Usage: ![roll|dice] [expression]\n"
+                + "for example: !roll 1d20 + 5 [to hit]"
+                + "[comment]: this is ignored"
+                + "2d20khX: keep the X highest dice"
+                + "2d20klX: keep the X lowest dice"
+                + "4d6r<X: reroll every die lower than X"
+                + "4d6ro<X: reroll every die lower than X, but only once"
+                + "1d10!: exploding die - every time you roll a crit, add an extra die", 2, null, "!roll", "!dice");
     }
 
     @Override
