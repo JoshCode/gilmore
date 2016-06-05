d39322b57a569a8d825bb6e84d4ed1916321bea5
JoshCode <joshua.b.slik@gmail.com>
2016-06-05 18:01:39 +0200

    Add MessageDeleter class

---
+++ src/main/java/nl/codefox/gilmore/util/MessageDeleter.java
@@ -0,0 +1,45 @@
+package nl.codefox.gilmore.util;
+
+import net.dv8tion.jda.entities.Message;
+
+/**
+ * @author Joshua Slik
+ */
+public class MessageDeleter implements Runnable {
+
+    private Message message;
+    private long milis;
+    private long targetTime;
+
+    public MessageDeleter(Message message) {
+        this.message = message;
+        this.milis = 5000;
+        targetTime = System.nanoTime() / 1_000_000 + milis;
+
+        new Thread(this).start();
+    }
+
+    public MessageDeleter(Message message, long milis) {
+        this.message = message;
+        this.milis = milis;
+        targetTime = System.nanoTime() / 1_000_000 + milis;
+
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        boolean deleted = false;
+        while(!deleted)
+        if (System.nanoTime() / 1_000_000 > targetTime) {
+            message.deleteMessage();
+            deleted = true;
+        } else {
+            try {
+                Thread.sleep(milis);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
