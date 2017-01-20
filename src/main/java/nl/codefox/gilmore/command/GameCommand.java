8894c6a4e33497b1e45aca2a5fa428355417c43f
Joshua Slik <joshua.b.slik@gmail.com>
2017-01-20 22:49:04 +0100

    Add command !game subscribed
    People wanted to see all the games they were subscribed to

--- src/main/java/nl/codefox/gilmore/command/GameCommand.java
+++ src/main/java/nl/codefox/gilmore/command/GameCommand.java
@@ -1,12 +1,6 @@
 package nl.codefox.gilmore.command;
 
-import nl.codefox.gilmore.command.game.Game;
-import nl.codefox.gilmore.command.game.GameCreateCommand;
-import nl.codefox.gilmore.command.game.GameHostCommand;
-import nl.codefox.gilmore.command.game.GameListCommand;
-import nl.codefox.gilmore.command.game.GameRemoveCommand;
-import nl.codefox.gilmore.command.game.GameSubscribeCommand;
-import nl.codefox.gilmore.command.game.GameUnsubscribeCommand;
+import nl.codefox.gilmore.command.game.*;
 import nl.codefox.gilmore.database.GilmoreDatabase;
 
 import java.util.ArrayList;
@@ -24,6 +18,7 @@ public class GameCommand extends GilmoreCommand {
         this.addSubCommand(new GameHostCommand());
         this.addSubCommand(new GameSubscribeCommand());
         this.addSubCommand(new GameUnsubscribeCommand());
+        this.addSubCommand(new GameSubscribedCommand());
         load();
     }
 
