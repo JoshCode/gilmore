1ad37aaa861f74e6743a1355cae58afafcc61e72
Michael Sarchet <mikesarchet@gmail.com>
2017-01-19 16:54:03 -0800

    Don't rebuild the welcome message for every user

--- src/main/java/nl/codefox/gilmore/listener/ConnectionListener.java
+++ src/main/java/nl/codefox/gilmore/listener/ConnectionListener.java
@@ -4,19 +4,22 @@ import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class ConnectionListener extends ListenerAdapter {
+    private String templatedString;
 
-    @Override
-    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+    public ConnectionListener () {
         StringBuilder builder = new StringBuilder();
-
-        builder.append(String.format("**Critical Role Discord Chat Rules**\n", event.getMember().getAsMention()));
+        builder.append("**Critical Role Discord Chat Rules**\n");
         builder.append("```#1 - Each channel says at the top if Critical Role spoilers are allowed or not. NO SPOILERS IN GENERAL CHAT.\n");
         builder.append("#2 - Don't Be a Dick. Otherwise, WELCOME!!!\n");
         builder.append("#3 - As long as you follow the above rules, we have chat channels for everything.  Playing games (Overwatch, Tabletop Simulatior, Rocket League, etc.), organizing DND/RPG games (LFG), discussing Critical Role or Game of Thrones or artwork or programming. We do it all in this community!\n");
         builder.append("#4 - If it is Thursday, we discuss the show #live in the channel #live, come join us there!```");
         builder.append("*I am a bot, and this message was automatic. Is it Thursday yet?*");
+        templatedString = builder.toString();
+    }
 
-        event.getMember().getUser().getPrivateChannel().sendMessage(builder.toString());
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        event.getMember().getUser().getPrivateChannel().sendMessage(templatedString);
     }
 
 }
