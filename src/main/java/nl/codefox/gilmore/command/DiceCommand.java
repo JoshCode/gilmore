b8dc8e4d8b4ee29a1dc20e531d21c8bde107e3fa
lclc98 <liam.c14785@gmail.com>
2017-02-28 12:23:01 +1000

    Added !roll stats

--- src/main/java/nl/codefox/gilmore/command/DiceCommand.java
+++ src/main/java/nl/codefox/gilmore/command/DiceCommand.java
@@ -24,7 +24,8 @@ public class DiceCommand extends GilmoreCommand {
 				+ "2d20klX: keep the X lowest dice\n"
 				+ "4d6r<X: reroll every die lower than X\n"
 				+ "4d6ro<X: reroll every die lower than X, but only once\n"
-				+ "1d10!: exploding die - every time you roll a crit, add an extra die";
+				+ "1d10!: exploding die - every time you roll a crit, add an extra die\n"
+				+ "stats: 6x 4d6 drop the lowest";
 	}
 
 	@Override
@@ -44,22 +45,32 @@ public class DiceCommand extends GilmoreCommand {
 
 	@Override
 	public void process(String command, String[] args, TextChannel channel, User author, MessageReceivedEvent event) {
-		String expression = "";
+        if (args.length > 0 && args[0].equalsIgnoreCase("stats")) {
+			StringBuilder builder = new StringBuilder();
+			Dice dice = new Dice("4d6kh3");
+			for (int i = 0; i < 6; i++) {
+				builder.append(String.format("%s = %d\n", dice.getBreakdown(), dice.roll()));
+			}
 
-		for (int i = 0; i < args.length; i++) {
-			expression += " " + args[i];
+			channel.sendMessage(builder.toString()).queue();
+		} else {
+			String expression = "";
+
+			for (int i = 0; i < args.length; i++) {
+				expression += " " + args[i];
+			}
+			expression = expression.substring(1);
+
+			Dice dice = new Dice(expression);
+			int result = dice.roll();
+
+			String msg = String.format("[%s] %s = %d", author.getAsMention(), dice.getBreakdown(), result);
+			if (msg.length() > 500) {
+				msg = msg.substring(0, 500);
+				msg += "[This message got cut off because it is too long.]";
+			}
+
+			channel.sendMessage(msg).queue();
 		}
-		expression = expression.substring(1);
-
-		Dice dice = new Dice(expression);
-		int result = dice.roll();
-
-		String msg = String.format("[%s] %s = %d", author.getAsMention(), dice.getBreakdown(), result);
-		if (msg.length() > 500) {
-			msg = msg.substring(0, 500);
-			msg += "[This message got cut off because it is too long.]";
-		}
-
-		channel.sendMessage(msg).queue();
-	}
+    }
 }
