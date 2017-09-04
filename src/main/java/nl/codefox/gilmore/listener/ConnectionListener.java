b32aa9a47b856093b36cdb700d86ef884dd66ab7
lclc98 <liam.c14785@gmail.com>
2017-09-04 15:00:57 +1000

    Removed CriticalRole command Updated to latest JDA

--- src/main/java/nl/codefox/gilmore/listener/ConnectionListener.java
+++ src/main/java/nl/codefox/gilmore/listener/ConnectionListener.java
@@ -16,9 +16,9 @@ public class ConnectionListener extends ListenerAdapter {
 		builder.append("#4 - If it is Thursday, we discuss the show #live in the channel #live, come join us there!```");
 		builder.append("*I am a bot, and this message was automatic. Is it Thursday yet?*");
 
-		if(!event.getMember().getUser().hasPrivateChannel())
-			event.getMember().getUser().openPrivateChannel();
-		event.getMember().getUser().getPrivateChannel().sendMessage(builder.toString());
+		event.getMember().getUser().openPrivateChannel().queue((channel)->{
+			channel.sendMessage(builder.toString()).queue();
+		});
 	}
 
 }
